6615d6be000d468af3b9057369749a8925d2af4a
Alan Mir <alanpaulphotos@gmail.com>
2022-06-28 11:42:40 -0500

    Created and styled about page with new headshot image

--- portfolio/src/containers/About/About.js
+++ portfolio/src/containers/About/About.js
@@ -1,11 +1,29 @@
 import React from 'react';
 
+import headshot from '../../assests/headshot.jpg';
 import "./About.scss";
 
 const About = () => {
     return (
         <div className='about'>
-            About
+            <img src={headshot} alt='Profesional Portrait of Gerardo Lopez' />
+
+            <div className='bio'>
+                <h2>About Me</h2>
+
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Faucibus et molestie ac feugiat sed lectus vestibulum mattis ullamcorper.
+                    Dolor sit amet consectetur adipiscing elit duis tristique sollicitudin nibh.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Faucibus et molestie ac feugiat sed lectus vestibulum mattis ullamcorper.
+                    Dolor sit amet consectetur adipiscing elit duis tristique sollicitudin nibh.
+                </p>
+            </div>
         </div>
     )
 };
