d98fc96ae8c7daff1526cb22756d0ba3af042c27
Alan Mir <alanpaulphotos@gmail.com>
2022-06-28 14:10:03 -0500

    Added svg to main title

--- portfolio/src/containers/Home/Home.js
+++ portfolio/src/containers/Home/Home.js
@@ -11,7 +11,13 @@ const Home = () => {
             <div className='h-title'>
                 <img className='mainImg' src={arandas} alt="Road map of city" />
 
-                <h1>Gerardo Lopez <span>Arquitecto</span></h1>
+                <h1>Gerardo Lopez <span>Arquitecto</span><svg preserveAspectRatio="xMidYMid meet" data-bbox="0 0 252.492 252.492" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 252.492 252.492" role="presentation" aria-hidden="true">
+                    <g>
+                        <path d="M252.492 117.246H135.246V0h-18v117.246H0v18h117.246v117.246h18V135.246h117.246v-18z"></path>
+                    </g>
+                </svg></h1>
+
+
             </div>
 
             <div className='h-text'>
