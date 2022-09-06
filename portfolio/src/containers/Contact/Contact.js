64c8ffc2620b490cb283c6c145767977c54bd9c4
Alan Mir <alanpaulphotos@gmail.com>
2022-09-06 16:22:05 -0400

    Added formik verification

--- portfolio/src/containers/Contact/Contact.js
+++ portfolio/src/containers/Contact/Contact.js
@@ -1,25 +1,68 @@
 import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
+import { useFormik } from 'formik';
 
 import email from "../../assests/envelope.png";
 import phone from "../../assests/telephone.png";
 import pin from "../../assests/pin.png";
 import "./Contact.scss";
 
+
+const validate = values => {
+    const errors = {};
+    if (!values.user_name) {
+        errors.user_name = 'Required *';
+    } else if (values.user_name.length > 15) {
+        errors.user_name = 'Must be 15 characters or less';
+    }
+
+    if (!values.user_email) {
+        errors.user_email = 'Required *';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.user_email)) {
+        errors.user_email = 'Invalid email address';
+    }
+
+    if (!values.user_message) {
+        errors.user_message = 'Required *';
+    } else if (values.user_message.length > 250) {
+        errors.user_message = 'Must be 250 characters or less'
+    }
+
+    return errors;
+};
+
+
 const Contact = () => {
     const [sent, setSent] = useState(false);
     const form = useRef();
 
-    const sendEmail = (e) => {
-        e.preventDefault();
-
-        emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, form.current, process.env.REACT_APP_PUBLIC_KEY)
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
-            });
-    };
+    const formik = useFormik({
+        initialValues: {
+            user_name: '',
+            user_email: '',
+            user_subject: '',
+            user_message: ''
+        },
+        validate,
+        onSubmit: values => {
+            emailjs
+                .sendForm(
+                    process.env.REACT_APP_SERVICE_ID,
+                    process.env.REACT_APP_TEMPLATE_ID,
+                    form.current,
+                    process.env.REACT_APP_PUBLIC_KEY
+                )
+                .then(
+                    (result) => {
+                        console.log(result.text);
+                        setSent(true);
+                    },
+                    (error) => {
+                        console.log(error.text);
+                    }
+                );
+        }
+    });
 
     return (
         <div className='contact'>
@@ -58,14 +101,26 @@ const Contact = () => {
                         <p>Estare en contacto pronto!</p>
                     </div>
                 ) : (
-                    <form ref={form} onSubmit={sendEmail}>
-                        <label>Nombre</label>
-                        <input type="text" name="user_name" />
-                        <label>Correo *</label>
-                        <input type="email" name="user_email" />
-                        <label>Mensaje</label>
-                        <textarea name="message" />
-                        <input className='submit' type="submit" value="Send" onClick={() => setSent(true)}/>
+                    <form ref={form} onSubmit={formik.handleSubmit}>
+                        <div className='label'>
+                            <label>Nombre </label>
+                            {formik.touched.user_name && formik.errors.user_name ? <div className='error'>{formik.errors.user_name}</div> : null}
+                        </div>
+                        <input type="text" id="user_name" name="user_name" onChange={formik.handleChange} value={formik.values.user_name} onBlur={formik.handleBlur} />
+
+                        <div className='label'>
+                            <label>Correo </label>
+                            {formik.touched.user_email && formik.errors.user_email ? <div className='error'>{formik.errors.user_email}</div> : null}
+                        </div>
+                        <input type="email" id="user_email" name="user_email" onChange={formik.handleChange} value={formik.values.user_email} onBlur={formik.handleBlur} />
+
+                        <div className='label'>
+                            <label>Mensaje </label>
+                            {formik.touched.user_message && formik.errors.user_message ? <div className='error'>{formik.errors.user_message}</div> : null}
+                        </div>
+                        <textarea id="user_message" name="user_message" onChange={formik.handleChange} value={formik.values.user_message} onBlur={formik.handleBlur} />
+
+                        <input className='submit' type='submit' value="Enviar" />
                     </form>
                 )}
             </div>
